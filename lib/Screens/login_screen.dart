df8b6efd66b5eb0f840038e973b83d321b644812
Abdullah Meda <abdullah.meda@gmail.com>
2021-12-28 17:14:34 +0400

    Implementing simple login UI

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
   static const id = '/auth';
+
+  Future<String?> _authUser(LoginData data) {
+    return Future.delayed(Duration(milliseconds: 2250)).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    return Future.delayed(Duration(milliseconds: 2250)).then((_) {
+      return null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello!'),
-        ),
-      ),
+    return FlutterLogin(
+      title: 'AMBER',
+      logo: const AssetImage('assets/logo.webp'),
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      loginAfterSignUp: false,
+      onRecoverPassword: (String) {},
+      onSubmitAnimationCompleted: () {},
     );
   }
 }
