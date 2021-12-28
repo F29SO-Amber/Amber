b9d987a5e1f85e90e23f591d38c8a054fd7ee810
Abdullah Meda <abdullah.meda@gmail.com>
2021-12-28 13:37:22 +0400

    Creating Skeleton code for #TODO screens

---
+++ lib/Screens/profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+  static const id = '/profile_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
