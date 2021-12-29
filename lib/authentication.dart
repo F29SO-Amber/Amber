3dab0a30b56311148a6c9a4f1bf1997b58b500da
Abdullah Meda <abdullah.meda@gmail.com>
2021-12-29 13:45:17 +0400

    Minor edit

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,6 +1,4 @@
-import 'package:amber/Screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_login/flutter_login.dart';
 
 class AuthenticationHelper {
