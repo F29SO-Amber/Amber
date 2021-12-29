dac580c527eb240bc1030d2f8e7ed5f90d6e6b12
John Mendez <jm2020@hw.ac.uk>
2021-12-29 13:11:55 +0400

    Firebase connection

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,18 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 
 class LoginScreen extends StatelessWidget {
   static const id = '/auth';
+  final _auth = FirebaseAuth.instance;
 
   Future<String?> _authUser(LoginData data) {
-    return Future.delayed(Duration(milliseconds: 2250)).then((_) {
-      return null;
+    return Future.delayed(Duration(milliseconds: 2250)).then((_) async {
+      try {
+        final User = _auth.signInWithEmailAndPassword(
+            email: data.name, password: data.password);
+      } catch (e) {
+        print(e);
+      }
     });
   }
 
   Future<String?> _signupUser(SignupData data) {
-    return Future.delayed(Duration(milliseconds: 2250)).then((_) {
-      return null;
+    return Future.delayed(Duration(milliseconds: 2250)).then((_) async {
+      try {
+        final newUser = await _auth.createUserWithEmailAndPassword(
+            email: data.name!, password: data.password!);
+      } catch (e) {
+        print(e);
+      }
     });
   }
 
