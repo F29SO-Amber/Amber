9fae92d8a485a09f80645fb05e09141123f6f837
Abdullah Meda <abdullah.meda@gmail.com>
2022-01-12 14:21:51 +0400

    Minor edit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         textSelectionTheme:
             const TextSelectionThemeData(cursorColor: Colors.orange),
       ),
-      initialRoute: ConvexAppBarDemo.id,
+      initialRoute: LoginScreen.id,
       routes: {
         LoginScreen.id: (context) => LoginScreen(),
         ConvexAppBarDemo.id: (context) => const ConvexAppBarDemo()
