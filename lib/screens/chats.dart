f40950ce4df2a0edc435081d6ce342ad331d8342
Abdullah Meda <abdullah.meda@gmail.com>
2022-01-15 16:59:17 +0400

    Adding modal bottom sheet for camera and gallery options

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,5 +1,7 @@
+import 'package:amber/widgets/profile_picture.dart';
 import 'package:flutter/material.dart';
 import 'package:amber/constants.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class ChatsPage extends StatefulWidget {
   const ChatsPage({Key? key}) : super(key: key);
@@ -14,101 +16,44 @@ class _ChatsPageState extends State<ChatsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: createRandomColor(),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 200,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                      width: 200,
-                      color: Colors.grey,
-                    ),
-                    Container(
-                      width: 200,
-                      color: Colors.blueGrey,
-                    ),
-                    Container(
-                      width: 200,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 200,
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 200,
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  children: <Widget>[
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.yellowAccent,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.yellowAccent,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 50,
-                      height: 100,
-                      color: Colors.red,
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 200,
-                child: GridView.count(
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 2,
-                  children: List.generate(50, (index) {
-                    return Container(
-                      child: Card(
-                        color: Colors.amber,
-                      ),
-                    );
-                  }),
-                ),
-              )
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text(
+          kAppName,
+          style: TextStyle(fontSize: 18, color: Colors.white),
         ),
       ),
+      body: GestureDetector(
+        child: const Center(child: Text('To be implemented!')),
+        onTap: () => showMaterialModalBottomSheet(
+          expand: false,
+          context: context,
+          backgroundColor: Colors.transparent,
+          builder: (context) => const ModalFit(),
+        ),
+      ),
+    );
+  }
+}
+
+class ModalFit extends StatelessWidget {
+  const ModalFit({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SafeArea(
+          top: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                ProfilePicture(side: 100, path: 'assets/camera.png'),
+                ProfilePicture(side: 100, path: 'assets/gallery.png'),
+              ],
+            ),
+          )),
     );
   }
 }
