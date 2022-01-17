56e80f8351c71f12537bc7995e560ff6684c5e20
aimee <aml10@hw.ac.uk>
2022-01-18 02:39:08 +0300

    update on backend

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:amber/constants.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:amber/screens/Messagepage.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseStorage storage = FirebaseStorage.instance;
 
 class ChatsPage extends StatefulWidget {
   const ChatsPage({Key? key}) : super(key: key);
@@ -10,9 +17,37 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  Color field2Color = Color(0xFF1F1A30);
+  double blurRadius2 = 0;
+  final myControllerPass = TextEditingController();
+  DateTime _now = DateTime.now();
+  String image_path = '';
+  var images_paths;
+  var imgaelink;
+  @override
+  Stream<String> downloadedUrl(String imageName) async* {
+    String downloadedUrl =
+    await storage.ref('test/$imageName').getDownloadURL();
+    yield downloadedUrl;
+  }
+
+  addData() async {
+    await FirebaseFirestore.instance
+        .collection('task')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('todo')
+        .add({
+      'task': myControllerPass.text,
+      'time': '${_now.hour}:${_now.minute}:${_now.second}.${_now.millisecond}'
+    });
+    myControllerPass.clear();
+    print("done");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       backgroundColor: Colors.black,
       appBar: AppBar(
         elevation: 15.0,
@@ -37,44 +72,179 @@ class _ChatsPageState extends State<ChatsPage> {
         ],
       ),
       body:  Container(
-        padding: const EdgeInsets.all(10.0),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30)
-          )
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Color(0xFF1F1A30),
+        child: Padding(
+          padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.06),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).size.height * 0,
+                    top: MediaQuery.of(context).size.height * 0.005),
+              ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .where('email',
+                      isNotEqualTo:
+                      FirebaseAuth.instance.currentUser)
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text("Error");
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    return ListView(
+                      children: snapshot.data!.docs
+                          .map((DocumentSnapshot document) {
+                        Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                        // images_paths = '${downloadedUrl(data['image'])}';
+                        // Future<String> downloadURLExample(img) async {
+                        //   String downloadURL = await firebase_storage
+                        //       .FirebaseStorage.instance
+                        //       .ref('test/${img}')
+                        //       .getDownloadURL()
+                        //       .toString();
+                        //   print(downloadURL);
+                        //   // print("000000000");
+                        //   // if(0){
+                        //   return downloadURL;
+                        //   // }
+                        //   // Within your widgets:
+                        //   // Image.network(downloadURL);
+                        //   // return downloadURL;
+                        // }
+
+                        // downloadURLExample(data['image']);
+
+                        // firebase_storage.Reference ref = firebase_storage
+                        //     .FirebaseStorage.instance
+                        //     .ref('test/images/${data['image']}');
+
+                        //     print(ref);
+                        //     print("done");
+
+                        //       image(ref) async {
+                        //         String location = await ref.getDownloadURL();
+                        //         return location;
+                        //       }
+                        //       print(image(ref));
+
+                        return InkWell(
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height *
+                                    0.02),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF39304d),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xFF39304d),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 0), // Shadow position
+                                ),
+                              ],
+                            ),
+                            child: ListTile(
+                              leading: Container(
+                                height:
+                                MediaQuery.of(context).size.height * 0.06,
+                                width:
+                                MediaQuery.of(context).size.height * 0.06,
+                                child: StreamBuilder(
+                                    stream: downloadedUrl('${data['image']}'),
+                                    builder: (context,
+                                        AsyncSnapshot<String> snap) {
+                                      if (snap.hasError) {
+                                        return Text("Error");
+                                      }
+                                      if (snap.connectionState ==
+                                          ConnectionState.waiting) {
+                                        return Center(
+                                          child: CircularProgressIndicator(),
+                                        );
+                                      }
+                                      imgaelink = snap.data!;
+                                      return CircleAvatar(
+                                        backgroundImage:
+                                        NetworkImage(snap.data!),
+                                      );
+                                    }),
+                              ),
+                              title: Text(
+                                data['name'],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              subtitle: Text(
+                                data['name'],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              trailing: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "12:00 am",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                        top: MediaQuery.of(context)
+                                            .size
+                                            .height *
+                                            0.008),
+                                    child: const CircleAvatar(
+                                      radius: 10,
+                                      backgroundColor: Color(0xFF1F1A30),
+                                      child: Text(
+                                        '1',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 10),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Messagepage(
+                                        chatman: data['email'],
+                                        chatmanname: data['name'])));
+
+                            // print(ref.getDownloadURL().toString());
+                            // print("done");
+                          },
+                        );
+                      }).toList(),
+                    );
+                  },
+                ),
+              ),
+              // ),
+            ],
+          ),
         ),
-        child: buildChats(),
       ),
     );
   }
   
 }
-
-Widget buildChats() =>ListView.builder(
-  physics: BouncingScrollPhysics(),
-  itemCount: 25,
-  itemBuilder: (context,index){
-    //final users= users[index];
-    return Container(
-      height: 75.0,
-      child: ListTile(
-        onTap: (){
-          Navigator.pushReplacementNamed(context, '/chat2');
-          //Navigator.pushNamed(context, '/home2');
-         // Navigator.of(context).push(MaterialPageRoute(builder: context)=>ChatPage(user:user))
-        },
-        leading: Image.asset('assets/img.png'),
-        title: Text('Vladamir Putin',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold
-        ),),
-        tileColor: Colors.black,
-
-      ),
-    );
-    }
-  );
 
