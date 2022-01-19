f61ef0f59fb2efc0482493978d3d0fd80dd4c3e7
Abdullah Meda <abdullah.meda@gmail.com>
2022-01-19 13:17:05 +0400

    Minor edit

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -16,53 +16,59 @@ class UserCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(5.0),
       child: GestureDetector(
         onTap: onPress,
-        child: Container(
-          height: 75,
-          width: double.infinity,
-          color: Colors.amber.shade100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              // color: Colors.amber.shade100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 15.0),
-                    child: ProfilePicture(side: 60, image: NetworkImage(user.profilePhotoURL)),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
-                      Text(
-                        user.name,
-                        style: GoogleFonts.dmSans(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.normal,
-                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 15.0),
+                        child: ProfilePicture(side: 40, image: NetworkImage(user.profilePhotoURL)),
                       ),
-                      const SizedBox(height: 2),
-                      Text(
-                        user.accountType,
-                        style: GoogleFonts.dmSans(
-                          fontSize: 13,
-                          color: Colors.black38,
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            user.name,
+                            style: GoogleFonts.dmSans(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                          Text(
+                            user.accountType,
+                            style: GoogleFonts.dmSans(
+                              fontSize: 10,
+                              color: Colors.black38,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 15.0),
+                    child: Icon(Icons.person_add),
+                  ),
                 ],
               ),
-              const Padding(
-                padding: EdgeInsets.only(right: 15.0),
-                child: Icon(Icons.person_add),
-              ),
-            ],
-          ),
+            ),
+            const Divider(
+              height: 3,
+            ),
+          ],
         ),
       ),
     );
