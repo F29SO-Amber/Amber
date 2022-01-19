e127ae26e707ca4b865d5821159cf10848322e7b
aimee <aml10@hw.ac.uk>
2022-01-19 12:13:47 +0300

    chat backend working

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -26,6 +26,8 @@ class _ChatPageState extends State<ChatPage> {
   final myControllerMsg = TextEditingController();
   DateTime _now = DateTime.now();
 
+  // String formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now1);
+
   // FirebaseAuth auth = FirebaseAuth.instance;
   @override
   addData() async {
@@ -33,6 +35,7 @@ class _ChatPageState extends State<ChatPage> {
       'token': '${FirebaseAuth.instance.currentUser!.email}|${widget.chatID}',
       'from': FirebaseAuth.instance.currentUser!.email,
       'to': widget.chatID,
+      // 'time': '${_now.hour}:${_now.minute}:${_now.second}.${_now.millisecond}',
       'time2': DateTime.now(),
       'message': myControllerMsg.text,
     });
@@ -40,6 +43,7 @@ class _ChatPageState extends State<ChatPage> {
     print("done");
     print(widget.chatID);
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -49,7 +53,10 @@ class _ChatPageState extends State<ChatPage> {
             children: [
               Padding(
                 padding: EdgeInsets.only(
-                    right: MediaQuery.of(context).size.height * 0.02),
+                    right: MediaQuery
+                        .of(context)
+                        .size
+                        .height * 0.02),
                 child: CircleAvatar(
                 ),
               ),
@@ -62,18 +69,47 @@ class _ChatPageState extends State<ChatPage> {
           ),
         ),
         body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
           color: Color(0xFF1F1A30),
           child: Column(
             children: [
+              // SizedBox(
+              //   height: 20,
+              // ),
+              // Padding(
+              //   padding: EdgeInsets.only(
+              //       bottom: MediaQuery.of(context).size.height * 0,
+              //       top: MediaQuery.of(context).size.height * 0.005),
+              //   child: Align(
+              //     alignment: Alignment.topLeft,
+              //     child: Padding(
+              //       padding: EdgeInsets.only(
+              //           top: MediaQuery.of(context).size.width * 0.06,
+              //           left: MediaQuery.of(context).size.width * 0.06),
+              //       // child: Text("${widget.chatmanname}",
+              //       //     style: TextStyle(
+              //       //         color: Colors.white,
+              //       //         fontWeight: FontWeight.bold,
+              //       //         fontSize: 30)),
+              //     ),
+              //   ),
+              // ),
               Expanded(
                 child: StreamBuilder<QuerySnapshot>(
                   stream: FirebaseFirestore.instance
                       .collection('messages')
                       .where('token', whereIn: [
-                    '${FirebaseAuth.instance.currentUser!.email}|${widget.chatID}',
-                    '${widget.chatID}|${FirebaseAuth.instance.currentUser!.email}'
+                    '${FirebaseAuth.instance.currentUser!.email}|${widget
+                        .chatID}',
+                    '${widget.chatID}|${FirebaseAuth.instance.currentUser!
+                        .email}'
                   ])
                       .orderBy('time2', descending: true)
                       .snapshots(),
@@ -92,8 +128,14 @@ class _ChatPageState extends State<ChatPage> {
 
                     return Padding(
                       padding: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width * 0.06,
-                          right: MediaQuery.of(context).size.width * 0.06),
+                          left: MediaQuery
+                              .of(context)
+                              .size
+                              .width * 0.06,
+                          right: MediaQuery
+                              .of(context)
+                              .size
+                              .width * 0.06),
                       child: ListView(
                         reverse: true,
                         children: snapshot.data!.docs
@@ -118,10 +160,19 @@ class _ChatPageState extends State<ChatPage> {
 
                           return Container(
                             margin: EdgeInsets.only(
-                                top: MediaQuery.of(context).size.height * 0.02,
-                                left: MediaQuery.of(context).size.width * lefts,
+                                top: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .height * 0.02,
+                                left: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width * lefts,
                                 right:
-                                MediaQuery.of(context).size.width * rights),
+                                MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width * rights),
                             decoration: BoxDecoration(
                               color: fieldColor,
                               borderRadius: BorderRadius.circular(20),
@@ -140,14 +191,18 @@ class _ChatPageState extends State<ChatPage> {
                               ),
                               trailing: Padding(
                                 padding: EdgeInsets.only(
-                                    bottom: MediaQuery.of(context).size.height *
+                                    bottom: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .height *
                                         0.008),
                                 child: Column(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   children: [
                                     Text(
                                       // DateTime.parse(timestamp.toDate().toString()),
-                                      "${DateFormat('hh:mm a').format(myDateTime)}",
+                                      "${DateFormat('hh:mm a').format(
+                                          myDateTime)}",
                                       style: TextStyle(color: dateColor),
                                     ),
                                   ],
@@ -163,9 +218,18 @@ class _ChatPageState extends State<ChatPage> {
               ),
               Container(
                 margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.02),
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * 0.15,
+                    top: MediaQuery
+                        .of(context)
+                        .size
+                        .height * 0.02),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .width * 0.15,
                 decoration: BoxDecoration(
                   color: Color(0xFF39304d),
                   // borderRadius: BorderRadius.circular(20),
@@ -208,6 +272,9 @@ class _ChatPageState extends State<ChatPage> {
                       ),
                       filled: true,
                       suffixIcon: InkWell(
+                        onTap: () {
+                          addData();
+                        },
                         child: Icon(
                           Icons.send_rounded,
                           color: Colors.white70,
