f40c296a82ecb47565041fd48aaa5587a2a399d7
Abdullah Meda <abdullah.meda@gmail.com>
2022-01-23 21:53:31 +0400

    Enhancing discovery page ui

--- lib/screens/discover.dart
+++ lib/screens/discover.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:amber/models/post.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 import 'package:amber/models/user.dart';
 import 'package:amber/screens/profile.dart';
 import 'package:amber/widgets/user_card.dart';
 import 'package:amber/utilities/constants.dart';
-import 'package:amber/services/auth_service.dart';
 import 'package:amber/services/database_service.dart';
 
 class DiscoverPage extends StatefulWidget {
@@ -29,27 +30,97 @@ class _DiscoverPageState extends State<DiscoverPage> {
         backgroundColor: Colors.amber, //sets the color to amber
         title: const Text(kAppName), //Title of the app
       ),
-      body: FutureBuilder(
-        future: DatabaseService.usersRef.get(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          //Show a list of the current users in the app
-          List<UserCard> list = [];
-          snapshot.data?.docs.forEach((doc) {
-            UserModel user = UserModel.fromDocument(doc);
-            if (user.id != AuthService.currentUser.uid) {
-              list.add(
-                UserCard(
-                  user: user,
-                  onPress: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ProfilePage(userUID: user.id)));
-                  },
-                ),
-              );
-            }
-          });
-          return ListView(children: list);
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Posts', style: kDarkLabelTextStyle.copyWith(fontSize: 30)),
+            ),
+            FutureBuilder(
+              future: DatabaseService.postsRef.get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return SizedBox(
+                    height: ((MediaQuery.of(context).size.width * 0.8) * 9) / 10,
+                    child: Swiper(
+                      outer: true,
+                      fade: 0.8,
+                      viewportFraction: 0.85,
+                      scale: 0.9,
+                      itemBuilder: (c, i) {
+                        PostModel post =
+                            PostModel.fromDocument((snapshot.data! as dynamic).docs[i]);
+                        return Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(post.imageURL), fit: BoxFit.cover),
+                            shape: BoxShape.rectangle,
+                          ),
+                        );
+                      },
+                      pagination: const SwiperPagination(alignment: Alignment.topCenter),
+                      itemCount: (snapshot.data! as dynamic).docs.length,
+                    ),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Users', style: kDarkLabelTextStyle.copyWith(fontSize: 30)),
+            ),
+            StreamBuilder(
+              stream: DatabaseService.usersRef.snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData) {
+                  var list = (snapshot.data as QuerySnapshot).docs.toList();
+                  return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: list.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      UserModel user = UserModel.fromDocument(list[index]);
+                      return UserCard(
+                        user: user,
+                        onPress: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ProfilePage(userUID: user.id)));
+                        },
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                // //Show a list of the current users in the app
+                // List<UserCard> list = [];
+                // snapshot.data?.docs.forEach((doc) {
+                //   UserModel user = UserModel.fromDocument(doc);
+                //   if (user.id != AuthService.currentUser.uid) {
+                //     list.add(
+                //       UserCard(
+                //         user: user,
+                //         onPress: () {
+                //           Navigator.push(
+                //               context,
+                //               MaterialPageRoute(
+                //                   builder: (context) => ProfilePage(userUID: user.id)));
+                //         },
+                //       ),
+                //     );
+                //   }
+                // });
+                // return ListView(children: list);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
