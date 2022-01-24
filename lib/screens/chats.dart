5066bbbcddfa0d6a5aa53e44cff74640fc5f4a88
Abdullah Meda <abdullah.meda@gmail.com>
2022-01-24 13:26:12 +0400

    Minor edit

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -3,8 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-import 'package:amber/utilities/constants.dart';
 import 'package:amber/screens/chat.dart';
+import 'package:amber/utilities/constants.dart';
 import 'package:amber/services/database_service.dart';
 
 final FirebaseStorage storage = FirebaseStorage.instance;
