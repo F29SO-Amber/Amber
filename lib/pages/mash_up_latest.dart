4038b1d167f99ae4808a23787f09a114dc68dec4
Abdullah Meda <abdullah.meda@gmail.com>
2022-02-15 21:26:35 +0400

    ui edit

--- lib/pages/mash_up_latest.dart
+++ lib/pages/mash_up_latest.dart
@@ -28,7 +28,7 @@ class _MashUpScreenState extends State<MashUpScreen> {
       child: CustomImage(
         height: side,
         width: side,
-        image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/image.png"),
+        image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/plus.png"),
         borderRadius: 0,
       ),
     );
@@ -44,13 +44,13 @@ class _MashUpScreenState extends State<MashUpScreen> {
           CustomImage(
             height: side,
             width: side / 2,
-            image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/image.png"),
+            image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/plus.png"),
             borderRadius: 0,
           ),
           CustomImage(
             height: side,
             width: side / 2,
-            image: (images.length >= 2) ? images[1] : const AssetImage("assets/image.png"),
+            image: (images.length >= 2) ? images[1] : const AssetImage("assets/plus.png"),
             borderRadius: 0,
           ),
         ],
@@ -70,13 +70,13 @@ class _MashUpScreenState extends State<MashUpScreen> {
               CustomImage(
                 height: side / 2,
                 width: side / 2,
-                image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/image.png"),
+                image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 2,
                 width: side / 2,
-                image: (images.length >= 2) ? images[1] : const AssetImage("assets/image.png"),
+                image: (images.length >= 2) ? images[1] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
             ],
@@ -86,17 +86,73 @@ class _MashUpScreenState extends State<MashUpScreen> {
               CustomImage(
                 height: side / 2,
                 width: side / 2,
-                image: (images.length >= 3) ? images[2] : const AssetImage("assets/image.png"),
+                image: (images.length >= 3) ? images[2] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 2,
                 width: side / 2,
-                image: (images.length >= 4) ? images[3] : const AssetImage("assets/image.png"),
+                image: (images.length >= 4) ? images[3] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget get5Layout() {
+    double side = MediaQuery.of(context).size.width;
+    return SizedBox(
+      height: side,
+      width: side,
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  CustomImage(
+                    height: side / 2,
+                    width: side / 2,
+                    image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/plus.png"),
+                    borderRadius: 0,
+                  ),
+                  CustomImage(
+                    height: side / 2,
+                    width: side / 2,
+                    image: (images.length >= 2) ? images[1] : const AssetImage("assets/plus.png"),
+                    borderRadius: 0,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  CustomImage(
+                    height: side / 2,
+                    width: side / 2,
+                    image: (images.length >= 3) ? images[2] : const AssetImage("assets/plus.png"),
+                    borderRadius: 0,
+                  ),
+                  CustomImage(
+                    height: side / 2,
+                    width: side / 2,
+                    image: (images.length >= 4) ? images[3] : const AssetImage("assets/plus.png"),
+                    borderRadius: 0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Center(
+            child: CustomImage(
+              height: side / 2,
+              width: side / 2,
+              image: (images.length >= 5) ? images[4] : const AssetImage("assets/plus.png"),
+              borderRadius: 0,
+            ),
+          )
         ],
       ),
     );
@@ -114,41 +170,19 @@ class _MashUpScreenState extends State<MashUpScreen> {
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/image.png"),
+                image: (images.isNotEmpty) ? images[0] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.length >= 2) ? images[1] : const AssetImage("assets/image.png"),
+                image: (images.length >= 2) ? images[1] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.length >= 3) ? images[2] : const AssetImage("assets/image.png"),
-                borderRadius: 0,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CustomImage(
-                height: side / 3,
-                width: side / 3,
-                image: (images.length >= 4) ? images[3] : const AssetImage("assets/image.png"),
-                borderRadius: 0,
-              ),
-              CustomImage(
-                height: side / 3,
-                width: side / 3,
-                image: (images.length >= 5) ? images[4] : const AssetImage("assets/image.png"),
-                borderRadius: 0,
-              ),
-              CustomImage(
-                height: side / 3,
-                width: side / 3,
-                image: (images.length >= 6) ? images[5] : const AssetImage("assets/image.png"),
+                image: (images.length >= 3) ? images[2] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
             ],
@@ -158,19 +192,41 @@ class _MashUpScreenState extends State<MashUpScreen> {
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.length >= 7) ? images[6] : const AssetImage("assets/image.png"),
+                image: (images.length >= 4) ? images[3] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.length >= 8) ? images[7] : const AssetImage("assets/image.png"),
+                image: (images.length >= 5) ? images[4] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
               CustomImage(
                 height: side / 3,
                 width: side / 3,
-                image: (images.length >= 9) ? images[8] : const AssetImage("assets/image.png"),
+                image: (images.length >= 6) ? images[5] : const AssetImage("assets/plus.png"),
+                borderRadius: 0,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              CustomImage(
+                height: side / 3,
+                width: side / 3,
+                image: (images.length >= 7) ? images[6] : const AssetImage("assets/plus.png"),
+                borderRadius: 0,
+              ),
+              CustomImage(
+                height: side / 3,
+                width: side / 3,
+                image: (images.length >= 8) ? images[7] : const AssetImage("assets/plus.png"),
+                borderRadius: 0,
+              ),
+              CustomImage(
+                height: side / 3,
+                width: side / 3,
+                image: (images.length >= 9) ? images[8] : const AssetImage("assets/plus.png"),
                 borderRadius: 0,
               ),
             ],
@@ -183,7 +239,7 @@ class _MashUpScreenState extends State<MashUpScreen> {
   @override
   void initState() {
     super.initState();
-    images.add(const AssetImage("assets/image.png"));
+    images.add(const AssetImage("assets/plus.png"));
     collageTypes.add(const CustomImage(
       side: 120,
       image: AssetImage("assets/1.png"),
@@ -197,6 +253,11 @@ class _MashUpScreenState extends State<MashUpScreen> {
     collageTypes.add(const CustomImage(
       side: 120,
       image: AssetImage("assets/4.png"),
+      borderRadius: 10,
+    ));
+    collageTypes.add(const CustomImage(
+      side: 120,
+      image: AssetImage("assets/5.png"),
       borderRadius: 10,
     ));
     collageTypes.add(const CustomImage(
@@ -244,12 +305,11 @@ class _MashUpScreenState extends State<MashUpScreen> {
                           onTap: () async {
                             if (index == images.length - 1) {
                               File? file = await ImageService.chooseFromGallery();
-                              setState(() {
-                                images.insert(
-                                  images.length - 1,
-                                  FileImage(file!),
-                                );
-                              });
+                              if (file != null) {
+                                setState(() => images.insert(images.length - 1, FileImage(file)));
+                              }
+                            } else if (index < images.length - 1) {
+                              setState(() => images.removeAt(index));
                             }
                           },
                         ),
@@ -260,7 +320,8 @@ class _MashUpScreenState extends State<MashUpScreen> {
                 if (index == 0) get1Layout(),
                 if (index == 1) get2Layout(),
                 if (index == 2) get4Layout(),
-                if (index == 3) get9Layout(),
+                if (index == 3) get5Layout(),
+                if (index == 4) get9Layout(),
                 SizedBox(
                   height: 120,
                   child: Swiper(
