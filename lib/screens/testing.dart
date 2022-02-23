dbbf323f16a0f4a6b01b9e852d1e757cc2d73be3
Abdullah Meda <abdullah.meda@gmail.com>
2022-02-23 13:29:47 +0400

    Revert "Whiteboard trail"
    This reverts commit 6cf0e30bb4f3fd1b15b7ee489bb9607fc4cf7a6c.

--- lib/screens/testing.dart
+++ lib/screens/testing.dart
@@ -1,6 +1,5 @@
 import 'package:amber/utilities/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:whiteboard/whiteboard.dart';
 
 class Testing extends StatefulWidget {
   const Testing({Key? key}) : super(key: key);
@@ -11,33 +10,10 @@ class Testing extends StatefulWidget {
 
 class _TestingState extends State<Testing> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    MaterialApp(
-      title: 'Whiteboard',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Whiteboard'),
-        ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Expanded(
-                child: WhiteBoard(),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return Scaffold(
+      appBar: kAppBar,
+      body: Container(),
     );
   }
 }
