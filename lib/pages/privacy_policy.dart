e9d05bfa953023ac8b7cc393f6a0d28ee4648118
aimee <aml10@hw.ac.uk>
2022-03-08 23:17:36 +0300

    minor fix

--- lib/pages/privacy_policy.dart
+++ lib/pages/privacy_policy.dart
@@ -34,7 +34,7 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
                   ),
                   Text(
                     'Last updated: March 08, 2022 \n '
-                        'This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Privacy Policy Generator.',
+                        'This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.',
                     style: GoogleFonts.poppins(
                         fontWeight: FontWeight.normal, fontSize: 15),
                   ),
