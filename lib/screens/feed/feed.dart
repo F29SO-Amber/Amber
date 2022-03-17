655d36b3c27bc0bb517b478250a6c79f1708a07f
John Mendez <jm2020@hw.ac.uk>
2022-03-17 08:54:29 +0400

    added Usability information for feed

--- lib/screens/feed/feed.dart
+++ lib/screens/feed/feed.dart
@@ -1,6 +1,6 @@
 import 'package:shimmer/shimmer.dart';
 import 'package:flutter/material.dart';
-
+import 'package:styled_text/styled_text.dart';
 import 'package:amber/user_data.dart';
 import 'package:amber/models/post.dart';
 import 'package:amber/widgets/post_widget.dart';
@@ -20,13 +20,30 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: kAppBar,
+      appBar: AppBar(
+        backgroundColor: kAppColor,
+        title: const Text(kAppName),
+        titleTextStyle: const TextStyle(
+            color: Colors.white, letterSpacing: 3, fontSize: 25.0),
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.info_outline,
+                size: 30,
+              ),
+              onPressed: () {
+                showDialogFunc(context,
+                    "Presenting the Future of Live Collaboration, Amber!");
+              })
+        ],
+      ),
       body: StreamBuilder(
         stream: DatabaseService.timelineRef
             .doc(UserData.currentUser!.id)
             .collection('timelinePosts')
             .snapshots()
-            .map((snapshot) => snapshot.docs.map((e) => PostModel.fromDocument(e))),
+            .map((snapshot) =>
+                snapshot.docs.map((e) => PostModel.fromDocument(e))),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             List list = (snapshot.data as dynamic).toList() as List;
@@ -101,4 +118,77 @@ class _FeedPageState extends State<FeedPage> {
       ),
     );
   }
+}
+
+showDialogFunc(context, title) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Material(
+          type: MaterialType.transparency,
+          child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(15),
+              height: 700,
+              width: 440,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    StyledText(
+                      text: """
+
+<bold>Let's Get Started</bold>
+The first page you are directed to is known as the "Feed Page". Here
+you will be able to see posts from every Amber account that you Follow.
+Do me a favor and try scrolling Horizontally on one of the Posts you see.
+
+<bold><head>Our main features</head></bold>
+<bold>Scroll a post Right to Mash-up!</bold>
+The backbone of Project Amber is the opportunity to MashUp posts from accounts you follow either by yourself,
+or with a group. This allows for seamless and enjoyable artistic ingenuity to flow from
+every user on our platform.
+
+<bold>Scroll a post Left to share your Thoughts!</bold>
+Praise and kind words make our day dont they? Send those you follow some words of encouragement by
+commenting on their posts. This allows for feedback and critiques on your work of art.
+
+<bold>Click the arrows Up/Down!</bold>
+These arrows tell the Account the general feedback to their work. Clicking the arrow pointing
+upwards gives the post a positive score and the other lowers it by a point.
+
+
+
+""",
+                      tags: {
+                        'bold': StyledTextTag(
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        'head': StyledTextTag(style: TextStyle(fontSize: 20))
+                      },
+                    )
+                  ],
+                ),
+              )),
+        ),
+      );
+    },
+  );
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -34,7 +34,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     chatsNavigatorKey,
     profileNavigatorKey,
   ];
-
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
